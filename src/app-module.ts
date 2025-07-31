ae811893d367fda023f09300b75d0d121bc2cc65
Jhones Jhonatas <jhones.jhonatas@outlook.com.br>
2025-07-30 21:43:29 -0300

    feat: create structure of project

---
+++ src/app-module.ts
@@ -0,0 +1,9 @@
+import { Module } from '@nestjs/common';
+
+import { ApiModule } from '@api/api-module';
+import { ApplicationModule } from '@application/application-module';
+
+@Module({
+  imports: [ApiModule, ApplicationModule],
+})
+export class AppModule {}
